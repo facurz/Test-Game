a6cb2dfacec7d13b78825b7a60967d9305bce3e0
Facundo Riva Zabala <facurz@hotmail.com>
2022-07-22 15:33:20 -0300

    level 1 terminado

--- src/level1/level1.js
+++ src/level1/level1.js
@@ -1,173 +1,120 @@
-
 // En los próximos problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-export function devolverString() {
-  // Tu código:
-
-  return;  
+export function devolverString(str) {
+    return str.toString();
 }
 
-export function suma() {
- // Tu código:
-
-  return;  
+export function suma(num1, num2) {
+    return num1 + num2;
 }
 
-export function resta() {
- // Tu código:
-
-  return;
+export function resta(num1, num2) {
+    return num1 - num2;
 }
 
-export function multiplica() {
-  // Tu código:
-
-  return;
+export function multiplica(num1, num2) {
+    return num1 * num2;
 }
 
-export function divide() { 
-  // Tu código:
-
-  return;
+export function divide(num1, num2) {
+    return num1 / num2;
 }
 
-export function sonIguales() {  
-  // Tu código:
-
-  return;  
+export function sonIguales(num1, num2) {
+    return num1 === num2;
 }
 
-export function tienenMismaLongitud() {  
-  // Tu código:
-
-  return;
+export function tienenMismaLongitud(str1, str2) {
+    return str1.toString().length === str2.toString().length;
 }
 
-export function menosQueNoventa() {  
-  // Tu código:
-
-  return;
+export function menosQueNoventa(num) {
+    return num < 90;
 }
 
-export function mayorQueCincuenta() {  
-  // Tu código:
-
-  return;
+export function mayorQueCincuenta(num) {
+    return num > 50;
 }
 
-export function obtenerResto() {  
-  // Tu código:
-
-  return;
+export function obtenerResto(num1, num2) {
+    return num1 % num2;
 }
 
-export function esPar() {
-  // Tu código:
-
-  return;
+export function esPar(num) {
+    return num % 2 === 0;
 }
 
-export function esImpar() {  
-  // Tu código:
-
-  return;
+export function esImpar(num) {
+    return num % 2 !== 0;
 }
 
-export function elevarAlCuadrado() {  
-  // Tu código:
-
-  return;
+export function elevarAlCuadrado(num) {
+    return num**2;
 }
 
-export function elevarAlCubo() { 
-  // Tu código:
-
-  return;
+export function elevarAlCubo(num) {
+    return num**3
 }
 
-export function elevar() {  
-  // Tu código:
-
-  return;
+export function elevar(num, exponent) {
+    return num**exponent
 }
 
-export function redondearNumero() {  
-  // Tu código:
-
-  return;
+export function redondearNumero(num) {
+    return Math.round(num);
 }
 
-export function redondearHaciaArriba() { 
-  // Tu código:
-
-  return;
+export function redondearHaciaArriba(num) {
+    return Math.ceil(num);
 }
 
-export function numeroRandom() {  
-  // Tu código:
-
-  return;
+export function numeroRandom() {
+    return Math.random();
 }
 
-export function esPositivo() {
-  // Tu código:
-  
-  return;
+export function esPositivo(num) {
+    if(num > 0) return 'Es positivo';
+    if(num< 0) return 'Es negativo';
+    if(num === 0) return false;
 }
 
-export function agregarSimboloExclamacion() {  
-  // Tu código:
-
-  return;
+export function agregarSimboloExclamacion(str) {
+    return `${str}!`;
 }
 
-export function combinarNombres() {
-  // Tu código:
-
-  return;
+export function combinarNombres(str1, str2) {
+    return `${str1} ${str2}`;
 }
 
-export function obtenerSaludo() {
-  // Tu código:
-
-  return;
+export function obtenerSaludo(nombre) {
+    return `Hola ${nombre}!`
 }
 
-export function obtenerAreaRectangulo() {  
-  // Tu código:
-
-  return;
+export function obtenerAreaRectangulo(alto, ancho) {
+    return alto * ancho;
 }
 
-
-export function retornarPerimetro(){
-  // Tu código:
-
-  return;
+export function retornarPerimetro(lado) {
+    return lado * 4;
 }
 
-
-export function areaDelTriangulo(){
-  // Tu código:
-
-  return;
+export function areaDelTriangulo(base, altura) {
+    return (base * altura) / 2;
 }
 
-
-export function deEuroAdolar(){
-  //Supongamos que 1 euro equivale a 1.20 dólares.
-  // Tu código:
-
-  return;
+export function deEuroAdolar(euro) {
+    return Number(euro) * 1.2;
 }
 
+export function esVocal(letra) {
+    let vocales = ['a', 'e', 'i', 'o', 'u'];
 
-export function esVocal(){
-  // Tu código:
+    if(letra.length === 0) return 'Dato incorrecto';
+    return (vocales.includes(letra)) ? 'Es vocal' : 'Dato incorrecto';
 
-  return;
+    
 }
