45f5a5586a62d5c229c2b962d8ce6ba9c4d0501a
Facundo Riva Zabala <facurz@hotmail.com>
2022-07-23 21:29:48 -0300

    test level 2 terminado

--- src/level2/level2.js
+++ src/level2/level2.js
@@ -1,94 +1,123 @@
 // No cambies los nombres de las funciones.
 
 export function obtenerMayor(x, y) {
-  // "x" e "y" son números enteros (int).
-  // Tu código:
-
-  return;
+    if (x > y) return x;
+    if (y > x) return y;
+    if (x === y) return x;
 }
 
 export function mayoriaDeEdad(edad) {
-  // Tu código:
-
-  return;
+    return edad >= 18 ? 'Allowed' : 'Not allowed';
 }
-  
+
 export function conection(status) {
-  //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
-  // Tu código:
-
-  return;
+    switch (status) {
+        case 1:
+            return 'Online';
+        case 2:
+            return 'Away';
+        case 3:
+            return 'Offline';
+        default:
+            return 'Offline';
+    }
 }
 
-export function saludo(idioma) {  
-  // Tu código:
-  
-  return;
+export function saludo(idioma) {
+    switch (idioma) {
+        case 'aleman':
+            return 'Guten Tag!';
+        case 'ingles':
+            return 'Hello!';
+        case 'mandarin':
+            return 'Ni Hao!';
+        default:
+            return 'Hola!';
+    }
 }
 
-export function colors(color) {  
-  //Usar el statement Switch.
-  // Tu código:
- 
+export function colors(color) {
+    switch (color) {
+        case 'blue':
+            return 'This is blue';
+        case 'red':
+            return 'This is red';
+        case 'green':
+            return 'This is green';
+        case 'orange':
+            return 'This is orange';
+        default:
+            return 'Color not found';
+    }
 }
 
 export function esDiezOCinco(numero) {
-  // Tu código:
-
-  return;
+    return numero === 10 || numero === 5;
 }
 
-export function estaEnRango(numero) {  
-  // Tu código:
-
-  return;
+export function estaEnRango(numero) {
+    return numero > 20 && numero < 50;
 }
 
 export function esEntero(numero) {
-  // Tu código:
-
-  return;
+    return Number.isInteger(numero);
 }
 
 export function fizzBuzz(numero) {
-  // Tu código:
- 
-  return;
+    if (numero % 5 === 0 && numero % 3 === 0) return 'fizzbuzz';
+    if (numero % 3 === 0) return 'fizz';
+    if (numero % 5 === 0) return 'buzz';
+    if (numero % 3 !== 0 && numero % 5 !== 0) return numero;
 }
 
 export function operadoresLogicos(num1, num2, num3) {
-  //La función recibe tres números distintos. 
-  // Tu código:
-  
-  return;
+    if (num1 === 0 || num2 === 0 || num3 === 0) return 'Error';
+    if (num1 < 0 || num2 < 0 || num3 < 0) return 'Hay negativos';
+    if (Math.sign(num1) === 1 && num1 > num2 && num1 > num3)
+        return 'Número 1 es mayor y positivo';
+    if (num3 > num1 && num3 > num2) return num3 + num1;
+    if (num1 || num2 || num3) return false;
 }
 
 export function esPrimo(numero) {
-  /// Tu código:
+    if (numero === 1) return false;
+    if (numero === 0) return false;
 
-  return;  
+    let primo = true;
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+            primo = false;
+            break;
+        }
+    }
+    return primo;
 }
 
-export function esVerdadero(valor){
-  // Tu código:
-
-  return;
+export function esVerdadero(valor) {
+    if (valor) return 'Soy verdadero';
+    if (!valor) return 'Soy falso';
 }
 
-export function tablaDelSeis(){
-  // Tu código:
-
-  return;
+export function tablaDelSeis() {
+    let tablaSeis = [];
+    for (let i = 0; i < 11; i++) {
+        tablaSeis = [...tablaSeis, i * 6];
+    }
+    return tablaSeis;
 }
 
-export function tieneTresDigitos(numero){
-  // Tu código:
-
-  return ;  
+export function tieneTresDigitos(numero) {
+    return numero.toString().length === 3;
 }
 
-export function doWhile(numero) {  
-  //Usar el bucle do ... while.
-  
-  return;
+export function doWhile(numero) {
+    let resultado = numero;
+    let i = 1;
+    
+    do {
+      resultado = resultado + 5;
+      i++;
+    } while (i < 9);
+    
+    return resultado;
 }
